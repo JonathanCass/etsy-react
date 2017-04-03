6df80f1858b9bcf658264119acdc649a973f5fc9
JonCass <JCass1980@gmail.com>
2017-04-03 14:26:45 -0700

    Working on errors

--- src/api/products.js
+++ src/api/products.js
@@ -1,14 +1,12 @@
 import store from '../store'
+import axios from 'axios'
 
-axios.get('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=invader_zim&includes=Images,Shop&callback=?').then(function(data){
-
-})
-	
-	export function displayItem() {
-		const products = data.results
-
-		store.dispatch({
-			type: 'DISPLAY_ITEM',
-			products : data.results
+export function displayItem() {
+	axios.get('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=invader_zim&includes=Images,Shop&callback=?')
+		.then(function(data){
+			store.dispatch({
+				type: 'DISPLAY_ITEM',
+				products : data.results
+			})
 		})
-	}
+}
